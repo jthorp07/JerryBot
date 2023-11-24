8ae9d5a4691cdf36b224ea8f9f972c8f392ff208
jathorp <jackthorp07@gmail.com>
2023-11-24 14:16:06 -0500

    spin command ported to typescript data portions are commented out since data is not yet ported

--- src/commands/spin_vod_forum.ts
+++ src/commands/spin_vod_forum.ts
@@ -1,5 +1,8 @@
-import { ChannelType, ChatInputCommandInteraction, Collection, Interaction, Message, SlashCommandBuilder } from "discord.js";
-import { ICommand, ICommandPermission } from "../types/discord_interactions";
+import { ChatInputCommandInteraction, SlashCommandBuilder, Collection, ChannelType, ForumChannel, AnyThreadChannel, GuildMember } from "discord.js";
+import { ICommand, ICommandExecute, ICommandPermission } from "../types/discord_interactions";
+// import { stdTempData, ptrTempData, addSpinData } from '../data/standard_submissions';
+
+// TODO: Uncomment data related items when /data/* gets ported to Typescript branch
 
 const VOD_REVIEW_FORUM = {
     key: 'vodforum',
@@ -10,15 +13,42 @@ const TIER_3_REVIEW_FORUM = {
     channelId: '1150160654485954590'
 }
 
-const freeVodReviewSpin = async (interaction: ChatInputCommandInteraction) => {
-    /** @type {ForumChannel} */
-    let channel = await interaction.guild?.channels.fetch(VOD_REVIEW_FORUM.channelId);
-    if (!channel || !(channel.type === ChannelType.GuildForum)) return;
-    let allThreads = await channel.threads.fetch();
 
-    let approvedTagId: string | undefined;
-    let deniedTagId: string | undefined;
-    let reviewedTagId: string | undefined;
+async function fetchThreadsFromForum(channel: ForumChannel): Promise<AnyThreadChannel<boolean>[]> {
+
+    const activeThreads = await channel.threads.fetch();
+    const archivedThreads = await channel.threads.fetch({
+        archived: {
+            fetchAll: true,
+        }
+    });
+
+    const activeThreadsCache = activeThreads.threads;
+    const archivedThreadsCache = archivedThreads.threads;
+
+    const threadBuf: AnyThreadChannel<boolean>[] = [];
+    for (const thread of activeThreadsCache) {
+        threadBuf.push(thread[1]);
+    }
+    for (const thread of archivedThreadsCache) {
+        threadBuf.push(thread[1]);
+    }
+    return threadBuf;
+}
+
+
+const freeVodReviewSpin: ICommandExecute = async (interaction) => {
+    const channel = await interaction.guild?.channels.fetch(VOD_REVIEW_FORUM.channelId);
+    if (!channel || !(channel.type === ChannelType.GuildForum)) return;
+    const allThreadsPromise = fetchThreadsFromForum(channel);
+
+    let approvedTagId;
+    let deniedTagId;
+    let reviewedTagId;
+
+    // stdTempData.reset();
+    let numEntries = 0;
+    let numPosts = 0;
 
     channel.availableTags.forEach(tag => {
         if (tag.name.toLowerCase() == "approved") approvedTagId = tag.id;
@@ -27,25 +57,45 @@ const freeVodReviewSpin = async (interaction: ChatInputCommandInteraction) => {
     });
 
     if (!approvedTagId || !reviewedTagId || !deniedTagId) {
-        await interaction.editReply({content:"This forum is missing one or multiple of the required tags:\n  \"approved\"\n  \"denied\"\n  \"reviewed\""});
+        await interaction.editReply({ content: "This forum is missing one or multiple of the required tags:\n  \"approved\"\n  \"denied\"\n  \"reviewed\"" });
         return;
     }
 
-    let buf = [] as {ownerId: string, ownerDisplayName: string, ownerUsername: string, clip: Message<true>}[];
+    let buf = [];
+    let ownerless = [];
+    const allThreads = await allThreadsPromise;
 
-    for (let i = 0; i < allThreads.threads.size; i++) {
-
-        let thread = allThreads.threads.at(i);
-        if (!thread) return;
+    console.log(`There are ${allThreads.length} threads`);
+    const currentDate = new Date(Date.now());
+    for (const thread of allThreads) {
 
         if (thread.appliedTags.includes(approvedTagId) && !thread.appliedTags.includes(reviewedTagId)) {
-            let owner = (await thread.fetchOwner())?.guildMember;
-            if (!owner) continue;
+            const ownerThreadMember = thread.fetchOwner()
+            const starterMessageFetch = thread.fetchStarterMessage()
+            const owner = (await ownerThreadMember)?.guildMember;
+            if (!owner) {
+                console.log('No owner');
+                ownerless.push(`https://discord.com/channels/${interaction.guildId}/${thread.id}`);
+                continue;
+            }
+            const patreonStatusMultiplier = owner.roles.cache.has("1094354496609591296") ? 4 : owner.roles.cache.has("1094354494218834040") ? 3 : owner.roles.cache.has("1094354485297561730") ? 2 : 1;
 
-            let starterMessage = await thread.fetchStarterMessage();
-            if (!starterMessage) continue;
 
-            let entries = owner.roles.cache.has("1094354496609591296") ? 4 : owner.roles.cache.has("1094354494218834040") ? 3 : owner.roles.cache.has("1094354485297561730") ? 2 : 1;
+            const postDate = thread.createdAt;
+            if (!postDate) return;
+            const differenceInMillis = currentDate.getTime() - postDate.getTime();
+            const wholeWeeks = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24 * 7));
+            const postAgeMultiplier = 1 + Math.floor(wholeWeeks / 2);
+
+            const entries = 1 * (patreonStatusMultiplier + postAgeMultiplier);
+            const starterMessage = await starterMessageFetch;
+            if (!starterMessage) {
+                console.log("No message fetch");
+                continue;
+            }
+            // stdTempData.addPostAge(currentDate.getTime() - thread.createdAt.getTime());
+            numEntries += entries;
+            numPosts++;
             for (let j = 0; j < entries; j++) {
                 buf.push({
                     ownerId: owner.id,
@@ -57,35 +107,49 @@ const freeVodReviewSpin = async (interaction: ChatInputCommandInteraction) => {
         }
     }
 
+    // addSpinData(numPosts, numEntries, 'std_vod');
+
     // __testWinRateDistributionForBuffer(buf); // Simulates 10,000 roles and logs win rates to determine fairness
 
     let winner = Math.floor(Math.random() * buf.length); // To test, move this logic into the commented function above
     let winningMessage = buf[winner].clip;
     let newContent = `The winner is: <@${buf[winner].ownerId}>`;
 
-    let coachChannel = await interaction.guild?.channels.fetch("1108357928655781888");
+    let links;
+    if (ownerless.length > 0) {
+        links = `The owners of the following threads may have left the server:\n`;
+        for (const link of ownerless) {
+            links = `${links}${link}\n`
+        }
+    }
+
+    let coachChannel = await interaction.guild?.channels.fetch("1161809001923747971");
+    const winningContent = `Raffle Winner Message:\n\n${winningMessage.content}`;
     if (!coachChannel || !coachChannel.isTextBased()) {
 
+
     } else {
-        coachChannel.send({ content: `Raffle Winner Message:\n\n${winningMessage.content}` })
+        coachChannel.send({ content: winningContent.length >= 2000 ? winningMessage.content : winningContent });
+        if (links) coachChannel.send({ content: links });
     }
     await interaction.editReply({ content: newContent });
 
 }
 
-const tierThreeVodSpin = async (interaction: ChatInputCommandInteraction) => {
 
+const tierThreeVodSpin: ICommandExecute = async (interaction) => {
 
-    // await interaction.editReply({content:'Tier 3 Vod Forum is Not Yet Implemented'});
-    // return;
-    /** @type {ForumChannel} */
-    let channel = await interaction.client.channels.fetch(TIER_3_REVIEW_FORUM.channelId);
+    const channel = await interaction.guild?.channels.fetch(TIER_3_REVIEW_FORUM.channelId);
     if (!channel || !(channel.type === ChannelType.GuildForum)) return;
-    let allThreads = await channel.threads.fetch();
+    const allThreadsPromise = fetchThreadsFromForum(channel);
 
     let approvedTagId;
     let deniedTagId;
     let reviewedTagId;
+
+    // ptrTempData.reset();
+    let numEntries = 0;
+    let numPosts = 0;
 
     channel.availableTags.forEach(tag => {
         if (tag.name.toLowerCase() == "approved") approvedTagId = tag.id;
@@ -94,23 +158,36 @@ const tierThreeVodSpin = async (interaction: ChatInputCommandInteraction) => {
     });
 
     if (!approvedTagId || !reviewedTagId || !deniedTagId) {
-        await interaction.editReply({content:"This forum is missing one or multiple of the required tags:\n  \"approved\"\n  \"denied\"\n  \"reviewed\""});
+        await interaction.editReply({ content: "This forum is missing one or multiple of the required tags:\n  \"approved\"\n  \"denied\"\n  \"reviewed\"" });
         return;
     }
 
     let buf = [];
-
-    for (let i = 0; i < allThreads.threads.size; i++) {
-
-        let thread = allThreads.threads.at(i);
-        if (!thread) return;
+    let ownerless = [];
+    const currentDate = new Date(Date.now());
+    const allThreads = await allThreadsPromise;
+    for (const thread of allThreads) {
 
         if (thread.appliedTags.includes(approvedTagId) && !thread.appliedTags.includes(reviewedTagId)) {
             let owner = (await thread.fetchOwner())?.guildMember;
-            if (!owner) continue;
+            if (!owner) {
+                console.log('No owner');
+                ownerless.push(`https://discord.com/channels/${interaction.guildId}/${thread.id}`);
+                continue;
+            }
 
             let starterMessage = await thread.fetchStarterMessage();
             if (!starterMessage) continue;
+
+            const postDate = thread.createdAt;
+            if (!postDate) return;
+            const differenceInMillis = currentDate.getTime() - postDate.getTime();
+            const wholeWeeks = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24 * 7));
+            const entries = 1 + Math.floor(wholeWeeks / 2);
+
+            // ptrTempData.addPostAge(currentDate.getTime() - thread.createdAt.getTime());
+            numEntries += entries;
+            numPosts++;
 
             buf.push({
                 ownerId: owner.id,
@@ -122,53 +199,45 @@ const tierThreeVodSpin = async (interaction: ChatInputCommandInteraction) => {
         }
     }
 
+    // addSpinData(numPosts, numEntries, 'ptr_vod');
+
     if (buf.length == 0) {
-        await interaction.editReply({content:'There are no tier three patreon submissions!'});
+        await interaction.editReply({ content: 'There are no tier three patreon submissions!' });
         return;
     }
 
     // __testWinRateDistributionForBuffer(buf); // Simulates 10,000 roles and logs win rates to determine fairness
-
     let winner = Math.floor(Math.random() * buf.length); // To test, move this logic into the commented function above
     let winningMessage = buf[winner].clip;
     let newContent = `The winner is: <@${buf[winner].ownerId}>`;
 
-    let coachChannel = await interaction.guild?.channels.fetch("1108357928655781888");
-    if (!coachChannel || !coachChannel.isTextBased()) {
+    let coachChannel = await interaction.guild?.channels.fetch("1161809001923747971");
 
-    } else {
-        coachChannel.send({ content: `Raffle Winner Message:\n\n${winningMessage.content}` })
-    }
-    await interaction.editReply({ content: newContent });
-
-
-}
-
-function __testWinRateDistributionForBuffer(buf: any[]) {
-    let winnerRates = new Collection<number, number>();
-    for (let i = 0; i < 10000; i++) {
-        let winner = Math.floor(Math.random() * buf.length);
-        if (winner === buf.length) winner -= 1;
-
-        if (winnerRates.get(winner) == undefined) {
-            winnerRates.set(winner, 1);
-        } else {
-            winnerRates.set(winner, winnerRates.get(winner) as number + 1);
+    let links;
+    if (ownerless.length > 0) {
+        links = `The owners of the following threads may have left the server:\n`;
+        for (const link of ownerless) {
+            links = `${links}${link}\n`
         }
     }
 
-    console.log("For buffer length " + buf.length);
-    console.log(JSON.stringify(winnerRates));
+    if (!coachChannel || !coachChannel.isTextBased()) {
+
+    } else {
+        coachChannel.send({ content: `Raffle Winner Message:\n\n${winningMessage.content}` });
+        if (links) coachChannel.send({ content: links });
+    }
+    await interaction.editReply({ content: newContent });
 }
 
-const sourceMap: Collection<String, (interaction: ChatInputCommandInteraction) => Promise<void>> = new Collection();
+const sourceMap = new Collection<String, ICommandExecute>();
 sourceMap.set(VOD_REVIEW_FORUM.key, freeVodReviewSpin);
 sourceMap.set(TIER_3_REVIEW_FORUM.key, tierThreeVodSpin);
 
 const command: ICommand = {
     data: new SlashCommandBuilder()
-        .setName('spinvodforum')
-        .setDescription('Spins a vod review forum to select a vod for review')
+        .setName('freevodreviewspin')
+        .setDescription('Executes a raffle')
         .addStringOption(option =>
             option.setName('type')
                 .setDescription('The type of raffle to do')
@@ -177,14 +246,35 @@ const command: ICommand = {
                     { name: 'Free Vod Review Forum', value: VOD_REVIEW_FORUM.key },
                     { name: 'Tier 3 Vod Review Forum', value: TIER_3_REVIEW_FORUM.key }
                 )) as SlashCommandBuilder,
-    execute: async (interaction) => {
+
+    async execute(interaction) {
+
         await interaction.deferReply();
-        let raffleType = interaction.options.getString('type');
-        let spinFunc = sourceMap.get(raffleType as string);
-        if (!spinFunc) return; // Should never happen
+        let raffleType = interaction.options.getString('type', true);
+        let spinFunc = sourceMap.get(raffleType);
+        if (!spinFunc) return;
         await spinFunc(interaction);
+
     },
     permissions: ICommandPermission.BOT_ADMIN,
 }
 
-export default command;
+
+// TypeScript doesn't like this guy
+
+// function __testWinRateDistributionForBuffer(buf) {
+//     let winnerRates = new Collection();
+//     for (let i = 0; i < 10000; i++) {
+//         let winner = Math.floor(Math.random() * buf.length);
+//         if (winner === buf.length) winner -= 1;
+
+//         if (winnerRates.get(winner) == undefined) {
+//             winnerRates.set(winner, 1);
+//         } else {
+//             winnerRates.set(winner, winnerRates.get(winner) + 1);
+//         }
+//     }
+
+//     console.log("For buffer length " + buf.length);
+//     console.log(JSON.stringify(winnerRates));
+// }
