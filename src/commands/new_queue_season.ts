304f9eea51c11964d3250b24bdc23b25e3f011c9
jathorp <jackthorp07@gmail.com>
2023-11-28 17:27:14 -0500

    new command start

--- src/commands/new_queue_season.ts
+++ src/commands/new_queue_season.ts
@@ -1,12 +1,15 @@
 import { SlashCommandBuilder } from "discord.js";
 import { ICommand, ICommandPermission } from "../types/discord_interactions";
+import { getMmrForAllUsers, addMmrUser, updateMmrUser } from "../util/database_options/firestore/db_mmr";
 
 
 const command: ICommand = {
     data: new SlashCommandBuilder()
-        .setName('seasonrefresh')
+        .setName('newseason')
         .setDescription('Performs all necessary data calculations for a new season, then responds with a list of instructions to finish starting a new season'),
     execute: async (interaction) => {
+
+
 
     },
     permissions: ICommandPermission.BOT_ADMIN
