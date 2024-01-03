3ff82fbc405927ac9b40aef12821970aca1f29ee
Jack Thorp <jackthorp07@gmail.com>
2024-01-03 15:15:58 -0500

    temp command to remove registered role

---
+++ src/commands/remove_role.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder } from "discord.js";
+import { ICommand, ICommandPermission } from "../types/discord_interactions";
+import { getLeaderboard } from "../util/database_options/firestore/db_leaderboard";
+
+const command: ICommand = {
+    data: new SlashCommandBuilder()
+        .setName('removerole')
+        .setDescription('Removes a role from all users in the server')
+        .addRoleOption(
+            option =>
+                option.setName('role')
+                    .setDescription('Role to remove')
+                    .setRequired(true)
+        ) as SlashCommandBuilder,
+    execute: async (interaction) => {
+        await interaction.reply({ content: 'Removing role from all users...' });
+        const role = interaction.options.getRole('role', true).id;
+        const lastLeaderboard = await getLeaderboard();
+        for (const user of lastLeaderboard) {           
+            interaction.guild?.members.fetch(user.discordId).then(member => member.roles.remove(role)).catch((err) => {if (err) console.error(err)});  
+        }
+    },
+    permissions: ICommandPermission.BOT_ADMIN
+}
+
+export default command;
