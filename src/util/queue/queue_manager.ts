a976e2fa4c48d93451d40bf5bd6fd384fdb5285c
Jack Thorp <jackthorp07@gmail.com>
2024-03-01 23:32:23 -0500

    started a QueueManager class

--- src/util/queue/queue_manager.ts
+++ src/util/queue/queue_manager.ts
@@ -1,5 +1,6 @@
 import { Snowflake } from "discord.js";
 import { SetQueue } from "./set_queue";
+import { QueueGame } from "./queue_game";
 
 export type WCAQueue = "classic";
 const queues = new Map<WCAQueue, SetQueue>();
@@ -9,3 +10,15 @@ export function enqueue(queue: WCAQueue, user: Snowflake) {
     const result = queues.get(queue)?.enqueue(user);
 }
 
+class QueueManager {
+
+    private queues: Map<WCAQueue, SetQueue> = new Map();
+    private games: Map<WCAQueue, QueueGame[]> = new Map();
+
+    constructor() {
+        this.queues.set("classic", new SetQueue())
+    }
+
+}
+
+export const queueManager = new QueueManager();
