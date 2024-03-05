8737ed03a5b76f1955a638b082e8a7256350ab85
Jack Thorp <jackthorp07@gmail.com>
2024-03-05 12:42:00 -0500

    started matchmaking algorithm in makeTeams() method

--- src/util/queue/queue_game.ts
+++ src/util/queue/queue_game.ts
@@ -1,6 +1,7 @@
 import { Snowflake } from "discord.js";
 import { EventEmitter } from "node:events";
 import { QueueEvent } from "./queue";
+import { FirebaseUserMmr, mmrManager } from "../database_options/firestore/db_mmr";
 
 type QueuePlayer = {
     discordId: Snowflake;
@@ -21,11 +22,31 @@ export class QueueGame {
     }
 
     private async makeTeams(members: Snowflake[]) {
+        const players: FirebaseUserMmr[] = [];
+        const promises = [];
+        for (const id of members) {
+            promises.push((async () => {
+                const player = await mmrManager.getUser(id);
+                if (!player) throw new Error("player does not exist");
+                players.push(player);
+            }).call(this));
+        }
+        await Promise.all(promises).catch(err => {
+            console.error(err);
+            this.cancel();
+            return;
+        });
+
+        players.sort((a, b) => {
+            return a.mmr - b.mmr;
+        });
         
+
+
     }
 
     private async cancel() {
-
+        this.events.emit(QueueEvent.GameOver, this.id);
     }
 
     async endGame(winningTeam: 1 | 2) {
