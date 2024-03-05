1d7308133c7214cda68655707132eb8cb9f59824
Jack Thorp <jackthorp07@gmail.com>
2024-03-05 14:43:21 -0500

    join now functional aside from registration check

--- src/buttons/join_queue.ts
+++ src/buttons/join_queue.ts
@@ -1,16 +1,25 @@
 import { ButtonBuilder, ButtonStyle } from "discord.js";
 import { IButton, ICommandPermission } from "../types/discord_interactions";
-import { WCAQueue } from "../util/queue/queue_manager";
+import { WCAQueue, queueManager } from "../util/queue/queue_manager";
 
 const customId = "joinq" as const;
 
 const button: IButton = {
     customId: customId,
-    execute: async (interaction) => {
+    execute: async (interaction, idArgs) => {
         await interaction.deferReply({ ephemeral: true });
         if (interaction.user.bot) {
-            await interaction.editReply({ content: "Bots cannot enter queues." })
-        } 
+            await interaction.editReply({ content: "Bots cannot enter queues." });
+            return;
+        }
+        try {
+            const queueName = idArgs[1] as WCAQueue;
+            queueManager.enqueue(interaction.user.id, queueName);
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+        
     },
     button: (queue: WCAQueue) => {
         return new ButtonBuilder()
