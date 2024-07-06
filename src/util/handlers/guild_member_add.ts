e077e6c03a7adfdd772f78931d5adb85234ac774
jthorp07 <jackthorp07@gmail.com>
2024-07-06 15:05:59 -0400

    Bot should now sync patreon role across servers

--- src/util/handlers/guild_member_add.ts
+++ src/util/handlers/guild_member_add.ts
@@ -1,15 +1,22 @@
 import { Events, GuildMember } from "discord.js";
 import { IEventHandler } from "../../types/event_handler";
 
+const PATREON_ROLE_ID = "1152001445390983218" as const;
+
 const eventHandler: IEventHandler = {
     event: Events.GuildMemberAdd,
     handlerFactory(client, checkPerms) {
         return async (member: GuildMember) => {
-            // TODO: Implement
+            if (member.guild.id === process.env.PATREON_SERVER) {
+                const mainGuild = await client.guilds.fetch(process.env.WORTHY_SERVER || "");
+                const mainGuildMember = await mainGuild.members.fetch(member.id);
+                await mainGuildMember.roles.add(PATREON_ROLE_ID);
+                console.log(`[Bot]: Patreon role added to new Patreon member ${member.id}`);
+            }
             return;
         }
     },
-    useHandler: false
+    useHandler: true
 }
 
 export default eventHandler;
